2cf13c568f3b150c9ecf2e699c111430ecf6ab73
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-08-28 17:25:38 -0300

    Entendendo a estrutura em Java e o uso de comentários.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
-public class App {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+public class App { // Estrutura padrão em Java, criando uma classe pública (que todos os arquivos podem utilizar o que está dentro dela)
+                        
+    public static void main(String[] args){ // Estrutura com classe estática nomeada void na principal(main), que declara as Strings(cacacteres/textos) e as chaves(args)
+        System.out.println("Hello, World!"); // Exibe a mensagem de "Hello Word" no terminal
     }
 }
+
+/**
+ * 
+ * Maneira chamada doc para adicionar comentários mais longos e informativos
+ * 
+ */
+
+/*
+    Forma de adicionar comentários sem utilizar o modelo doc
+*/
